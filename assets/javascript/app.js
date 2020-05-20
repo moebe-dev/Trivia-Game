c6e4ed99e730dc75d4c7008ca55380bcfd753a1f
Muhammad Ali <qtyeert@yahoo.com>
2020-05-20 07:43:55 -0500

    Test For Back-End

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -155,7 +155,7 @@ $(document).ready(function () {
     function displayImage() {
         if (count === 0) {
             $("#image-holder").show();
-            $("#image-holder").html('<img src="../images/Texas Austin.jpg">');
+            $("#image-holder").html('<img src="assets\images\Texas Austin.jpg">');
         }
         else if (count === 1) {
             $("#image-holder").show();
